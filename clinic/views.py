caf0c962791b8a2731e025e338a55f04b2462f96
agyesam12 <agyesam12@gmail.com>
2025-01-11 20:41:50 +0000

    added a signup view function

--- clinic/views.py
+++ clinic/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib import messages
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            messages.success(request, f"Acount created successfully Login Now! ...")
+            return redirect('signin')
+        else:
+            messages.info(request, f"Something went wrong try again ..")
+    else:
+        form = SignupForm()
+    context = {'form': form}
+    return render(request,'signup.html', context)
+
 def contact(request):
     return render(request, 'contact.html')
 
 def about(request):
     return render(request, 'about.html')
-from django.shortcuts import render
+
 
 def appointments(request):
     return render(request, 'appointments.html')
