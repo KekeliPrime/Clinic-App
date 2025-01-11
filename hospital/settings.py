9eacbfc24592663e3fd288e7ce01f03352a9f646
agyesam12 <agyesam12@gmail.com>
2025-01-11 20:58:33 +0000

    configured settings

--- hospital/settings.py
+++ hospital/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4bn&m)z7k2l8clnqsefypvy12^ax9yh7a)z3jfrlhizy@jvii-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
